1be2c546e56c8f871fff52804eb3d9adea733341
Ikmal Azman <opikmalazman@gmail.com>
2023-02-20 18:22:10 +0000

    Initial Commit

---
+++ UIActionSheet-SwiftUI/UIActionSheet_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIActionSheet_SwiftUIApp.swift
+//  UIActionSheet-SwiftUI
+//
+//  Created by Ikmal Azman on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct UIActionSheet_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
